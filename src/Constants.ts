f45036581697fc84ed09455b36572739ddcbdaaa
kongqingchao <kongqingchao@bixin.cn>
2023-05-20 00:51:34 +0800

    定时检查文件更新，并发送通知

---
+++ src/Constants.ts
@@ -0,0 +1,35 @@
+
+//小麦 长期交易策略
+export const longTermWheatChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//TQQQ 长期交易策略
+export const longTermTQQQChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+// 2倍做多纳指100ETF(QLD) 短期交易策略
+export const qldChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//纳指100 日内交易策略(V2)
+export const dayTradeV2ChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//纳指100 日内交易策略(V3)
+export const dayTradeV3ChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//纳指100 日内交易策略(V4)
+export const dayTradeV4ChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//纳指100 日内交易策略(V5)
+export const dayTradeV5ChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//QQQ 与 IWM 间风格轮动策略
+export const rotationChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//IWM 长期交易策略
+export const longTermIWMChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//纳指100 日内交易策略
+export const NQV4ChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+//标普500 日内交易策略
+export const sp500ChartFileName = '/Users/kongqingchao/IdeaProjects/SapienAlpha/wechatbot/source/testScanChart.png';
+
+export const disclaimer = '策略仅供参考，不构成任何投资建议。请务必阅读免责声明(http://webapp.sapienalpha.net/)';
